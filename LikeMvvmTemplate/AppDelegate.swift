442ebf412ffa4e90f0cc6d29b66d67467ebc2c6f
ysmdsty <ysmodesty@gmail.com>
2015-12-06 19:53:52 +0900

    add SwiftBond sample

--- LikeMvvmTemplate/AppDelegate.swift
+++ LikeMvvmTemplate/AppDelegate.swift
@@ -1,5 +1,24 @@
 import UIKit
 
+class Mock {
+    static func performDelay(delayTime: UInt64 ,closure: (()->())) {
+        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(delayTime * NSEC_PER_SEC))
+        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
+            closure()
+        }
+    }
+    static func dispatchAsyncMain(closure: (()->())){
+        dispatch_async(dispatch_get_main_queue()) {
+            closure()
+        }
+    }
+    static func dispatchAsyncGlobal(closure: (()->())){
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            closure()
+        }
+    }
+}
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
---
+++ LikeMvvmTemplate/SwiftBondViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class SwiftBondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
